0a242379735869fa96ee47e6c3e7e3fee7e4774a
Aronnnny <aron.cardoso1@gmail.com>
2024-04-18 23:51:50 -0300

    firstCommit

---
+++ src/productRegister.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using productRegister.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productRegister.Domain.Repositories
+{
+    internal interface IProductRepository : IRepositoryBase<Product>
+    {
+    }
+}
